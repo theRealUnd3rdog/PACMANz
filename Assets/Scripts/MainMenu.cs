02d4707b8b64dfc702cda8e607a64aaa10cbbdc5
Chamath Devmina <chamathdewmina25@gmail.com>
2023-06-25 22:21:14 +0530

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(3);
-    }
-
     public void Quit()
     {
         Application.Quit();
